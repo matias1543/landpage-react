af007382d309af560c5a4666bec74ddb6bf1201b
Matias <matias_julian@outlook.com>
2022-12-07 13:57:03 -0300

    fix menu burger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const NavBar = () => {
+
+    const [sideBarOpen, setSideBarOpen] = useState(false)
+
+    const showSideBar = (show) => {
+        setSideBarOpen(!show)
+    }
+
     return (
         <header>
             <div className="max">
@@ -14,9 +21,15 @@ export const NavBar = () => {
                     <a href="#"><h3>Contacto</h3></a>
                     <input type="checkbox" id="check" />
                     <label htmlFor="check">
-                        <i className="fa-solid fa-bars"></i>
+                        <i className="fa-solid fa-bars" onClick={ () => showSideBar(sideBarOpen)} ></i>
                     </label>
                 </nav>
+                <ul className={`menuNav ${sideBarOpen ? " showMenu" : ""}`}>
+                    <li><a href="#"><h3>Nosotros</h3></a></li>
+                    <li><a href="#"><h3>Servicios</h3></a></li>
+                    <li><a href="#"><h3>Clientes</h3></a></li>
+                    <li><a href="#"><h3>Contacto</h3></a></li>
+                </ul>
             </div>
         </header>
     )
