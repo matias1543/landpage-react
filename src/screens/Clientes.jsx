6cca0fc7d0b83490f400f6b307ffa7e6fe3504e1
Matias <matias_julian@outlook.com>
2022-12-14 17:30:43 -0300

    Fix version mobile carousel

--- src/screens/Clientes.jsx
+++ src/screens/Clientes.jsx
@@ -1,16 +1,55 @@
-import { forwardRef } from 'react';
+import { useState, forwardRef, useEffect } from 'react';
 import Slider from "react-slick";
 import { Cliente } from '../components/Cliente';
 import { clientes } from '../data/clientes';
 
 export const Clientes = forwardRef((props, ref) => {
     
-    const settings = {
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    };
+    const [width, setWidth] = useState(window.innerWidth);
+    
+    useEffect(() => {
+      const handleResizeWindow = () => setWidth(window.innerWidth);
+    
+      window.addEventListener("resize", handleResizeWindow);
+      return () => {
+        window.removeEventListener("resize", handleResizeWindow);
+      }
+    }, [])
+    
+    const cardsToShow = (width) => {
+        if(width <= 412) {
+            return {
+                infinite: true,
+                speed: 500,
+                arrows: false,
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        if(width <= 1054) {
+            return {
+                infinite: true,
+                speed: 500,
+                arrows: true,
+                dots: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }
+        else {
+            return {
+                infinite: true,
+                speed: 500,
+                arrows: true,
+                dots: true,
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        }
+    }
+
+    let settings = cardsToShow(width);
 
     return (
         <div id='container-one' ref={ref}>
